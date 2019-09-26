734fb79895eb7df425b7f1b7c0448678770e848b
Vinicius de Jesus <souza.vdj@gmail.com>
2019-09-26 16:21:51 -0300

    Commit inicial do projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<html>
+    <header>
+        <meta charset="utf-8">
+        <title>Smart Access</title>
+        <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    </header>
+    <body>
+        <header>
+
+            <?php echo '<h2>Cabeçalho</h2>'; ?>
+
+        </header>
+        <section id="Denis">
+            
+            <?php echo '<p>Corpo</p>'; ?>
+        
+        </section>
+        <footer>
+        
+            <?php echo '<p>Rodapé</p>'; ?>
+        
+        </footer>
+    </body>
+</html>
